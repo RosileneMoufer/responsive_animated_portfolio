dae51706b7a2c0036146274f108007a028fbc4fd
RosileneMoufer <rosilenemoufer@gmail.com>
2022-07-13 13:21:18 -0300

    organizing folders

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import '../main/main_screen.dart';
 
-import 'components/heighlights.dart';
-import 'components/home_banner.dart';
-import 'components/my_projects.dart';
-import 'components/recommendations.dart';
+import 'components/highlighted/highlighted.dart';
+import 'components/banner/home_banner.dart';
+import 'components/projects/my_projects.dart';
+import 'components/recommendation/recommendations.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
